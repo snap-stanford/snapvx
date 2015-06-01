14c69e1e4e188eb4a55df8604a4688f084ecdd1d
Rok Sosic <rok@cs.stanford.edu>
2015-06-01 14:56:13 -0700

    - updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from distutils.core import setup
 setup(name='snapvx',
-      version='0.1',
-      py_modules=['snapvx'],
-      )
+    version='0.1',
+    #data_files=[('Examples', [
+    #    'Examples/BulkLoadData.csv',
+    #    'Examples/BulkLoadEdges.edges',
+    #    'Examples/BulkLoading.py',
+    #    'Examples/HelloWorld.py',
+    #    'Examples/LaplacianRegularization.py'])],
+    py_modules=['snapvx']
+    )
 
