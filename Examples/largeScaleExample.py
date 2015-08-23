a8cc6e92e7feea3384fa240cfb1244ac8119da5e
David Hallac <hallac@madmax6.stanford.edu>
2015-08-23 13:30:47 -0700

    Added large scale example

---
+++ Examples/largeScaleExample.py
@@ -0,0 +1,31 @@
+from snapvx import *
+import numpy
+import time
+
+def laplace_reg(src, dst, data):
+    return (sum_squares(src['x'] - dst['x']), [])
+
+num_nodes = 100
+size_prob = 5000
+
+temp = GenRndDegK(num_nodes, 3)
+gvx = TGraphVX(temp)
+
+for i in range(num_nodes):
+    x = Variable(size_prob,name='x')
+    a = numpy.random.randn(size_prob)
+    gvx.SetNodeObjective(i, square(norm(x-a)))
+
+gvx.AddEdgeObjectives(laplace_reg)
+
+start = time.time()
+gvx.Solve(verbose=True)
+ellapsed = time.time() - start
+print ellapsed, "seconds; with ADMM"
+
+
+start = time.time()
+#gvx.Solve(useADMM=False)                                                                                             
+ellapsed = time.time() - start
+print ellapsed, "seconds; no ADMM"
+
