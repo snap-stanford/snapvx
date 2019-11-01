0b0b555fbd776c9e0dc3161fda59b1820db59c01
Thomas <thomas.gaudelet.16@ucl.ac.uk>
2019-11-01 13:49:34 -0700

    Update snapvx.py. It now only support python3 and cvxpy>=1.25.0. README.md file updated to reflect the change.

--- snapvx.py
+++ snapvx.py
@@ -21,8 +21,11 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from __future__ import print_function
+
 from snap import *
 from cvxpy import *
+
 
 import math
 import multiprocessing
